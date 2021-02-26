86818d2f817aa1a39992fa82e95a51061d9c9115
gga123 <61962097+gga123freakout@users.noreply.github.com>
2021-02-26 23:37:17 +0700

    update more.blade

--- resources/views/more.blade.php
+++ resources/views/more.blade.php
@@ -88,10 +88,12 @@
             <div class="col-lg-10 col-sm-10 col-10 my-5 text-center">
                 <h2><b>Attachments</b></h2>
                 <div class="row justify-content-center">
-                    <div class="col-lg-1 col-sm-1 text-center prevnext">
+                    <div class="col-lg-1 col-sm-1 text-center prevnext" style="position: relative;">
                         <a class="btn" href="#myCarousel" data-slide="prev" id="scheduleprevnext" title="go back"><i class="fa fa-lg fa-chevron-left"></i></a>
                     </div>
-                    <div class="col-lg-10 col-sm-10 text-center">
+
+                    <section class="carousel slide col-lg-10 col-sm-10 text-center" id="myCarousel" data-interval="false" style="right: -18px;">
+
                         <div class="carousel-inner text-center">
                             <div class="row row-equal carousel-item my-2 active">
                                 <div class="row justify-content-center">
@@ -110,8 +112,9 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-lg-1 col-sm-1 text-center prevnext">
+
+                    </section>
+                    <div class="col-lg-1 col-sm-1 text-center prevnext" style="position: relative;">
                         <a class="btn" href="#myCarousel" data-slide="next" id="scheduleprevnext" title="more"><i class="fa fa-lg fa-chevron-right"></i></a>
                     </div>
                 </div>
